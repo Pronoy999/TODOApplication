b36659fb116138b5098e6619e2331bc07a148145
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2018-04-27 12:31:52 +0530

    Added Activity to add category.

---
+++ app/src/main/java/com/pronoy/mukhe/todoapplication/Acitvities/AddCategoryActivity.java
@@ -0,0 +1,16 @@
+package com.pronoy.mukhe.todoapplication.Acitvities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pronoy.mukhe.todoapplication.R;
+
+public class AddCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_category);
+
+    }
+}
