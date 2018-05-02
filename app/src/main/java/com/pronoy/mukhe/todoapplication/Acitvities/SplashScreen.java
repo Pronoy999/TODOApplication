e8fc299803fb767a7a932fac672bd7ca0b82c4e4
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2018-05-02 11:38:02 +0530

    Fixed Notification's NULL Pointer Exception.

--- app/src/main/java/com/pronoy/mukhe/todoapplication/Acitvities/SplashScreen.java
+++ app/src/main/java/com/pronoy/mukhe/todoapplication/Acitvities/SplashScreen.java
@@ -21,11 +21,13 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         Constants.databaseController = new DatabaseController(getApplicationContext());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Constants.NOTIFICATION_CHANNEL=new NotificationChannel(String.valueOf(Constants.NOTIFICATION_CHANNEL_ID),
+            Constants.NOTIFICATION_CHANNEL=new NotificationChannel(String
+                    .valueOf(Constants.NOTIFICATION_CHANNEL_ID),
                     Constants.NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
             Constants.NOTIFICATION_CHANNEL.setDescription(Constants.NOTIFICATION_CHANNEL_DESCRIPTION);
             Constants.NOTIFICATION_MANAGER.createNotificationChannel(Constants.NOTIFICATION_CHANNEL);
         }
+        Constants.NOTIFICATION_MANAGER= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -36,9 +38,11 @@ public class SplashScreen extends AppCompatActivity {
                     SharedPreferences.Editor editor = sharedPreferences.edit();
                     editor.putBoolean(Constants.LOG_IN_STATUS, true);
                     editor.apply();
-                    changeActivityIntent = new Intent(SplashScreen.this, CategoryActivity.class);
+                    changeActivityIntent = new Intent(SplashScreen.this,
+                            CategoryActivity.class);
                 } else {
-                    changeActivityIntent = new Intent(SplashScreen.this, TodoActivity.class);
+                    changeActivityIntent = new Intent(SplashScreen.this,
+                            TodoActivity.class);
                 }
                 startActivity(changeActivityIntent);
                 finish();
